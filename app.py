b7bec6c8fe53ec018b33fa9d16b465225de4b5e1
Mohamed Nadeem <mnadeem.es@gmail.com>
2025-03-31 20:27:41 +0200

    Changes: - Aggregate average sensor value - Changed trigger to 1 minute

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
+from pyspark.sql.functions import from_json, col, window, avg, round, to_json, struct
 
 spark = SparkSession.builder.appName("SensorWindowAggregation").getOrCreate()
 
@@ -20,8 +20,25 @@ sensor_stream = data_stream.select(
     col("timestamp").cast("long").alias("timestamp"), col("value"), col("sensorId").alias("sensorId")
 )
 
-sensor_stream.writeStream \
+watermarked_stream = sensor_stream.withColumn("event_time", (col("timestamp") / 1000).cast("timestamp")) \
+    .withWatermark("event_time", "1 minute")
+
+agg_stream = watermarked_stream.groupBy(
+    col("sensorId"),
+    window(col("event_time"), "1 minute")
+).agg(round(avg("value")).alias("averageValue"))
+
+result_stream = agg_stream.select(
+    col("sensorId"),
+    (col("window.start").cast("long") * 1000).alias("windowStart"),
+    (col("window.end").cast("long") * 1000).alias("windowEnd"),
+    col("averageValue")
+)
+
+output_stream = result_stream.select(to_json(struct("sensorId", "windowStart", "windowEnd", "averageValue")).alias("value"))
+
+output_stream.writeStream \
     .outputMode("update") \
     .format("console").option("truncate", False)\
-    .trigger(processingTime="5 seconds") \
+    .trigger(processingTime="1 minute") \
     .start().awaitTermination()
